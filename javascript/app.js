d80bf284a6649be31a2c4709b7399139eb76c95e
denyanwardev <denyanwardev@gmail.com>
2023-01-25 18:16:43 +0800

    first step create application

---
+++ javascript/app.js
@@ -0,0 +1,62 @@
+//=============================
+// select items
+//=============================
+
+const form = document.querySelector(".shopping-form");
+const alert = document.querySelector(".alert");
+const shopping = document.getElementById("shopping");
+const submitBtn = document.querySelector(".submit-btn");
+const container = document.querySelector(".shop-container");
+const list = document.querySelector(".shop-list");
+const clearBtn = document.querySelector('.clear-btn');
+
+// --edit option
+let editElement;
+let editFlag = false;
+let editID = "";
+
+
+//=============================
+// event listener
+//=============================
+
+// --submit form
+form.addEventListener("submit", addItem);
+// clear list
+clearBtn.addEventListener("click", clearItems);
+// display items onload
+window.addEventListener("DOMContentLoaded", setupItems);
+
+
+//=============================
+// functions
+//=============================
+
+// --add item
+function addItem(e) {
+  e.preventDefault();
+  const value = shopping.value;
+  const id = new Date.getTime().toString();
+
+  if (value !== "" && !editFlag) {
+    const element = document.createElement("article");
+    let attr = document.createAttribute("data-id");
+    attr.value = id;
+    element.setAttributeNode(attr);
+    element.classList.add("grocery-item") // css
+    element.innerHTML = `
+      <p class="title">${value}</p>
+      <div class="btn-container">
+        <!-- edit btn -->
+        <button type="button" class="edit-btn">
+          <i class="fas fa-edit"></i>
+        </button>
+        <!-- delete btn -->
+        <button type="button" class="delete-btn">
+          <i class="fas fa-trash"></i>
+        </button>
+      </div>
+    `;
+  }
+}
+
